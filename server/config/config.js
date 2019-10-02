0bc14c1665543a6cb7c52088725f59b758e717a4
Miguel Angel <dhyama@gmail.com>
2019-10-02 12:01:04 +0200

    Guardar modificaciones en la configuración del proyecto

--- server/config/config.js
+++ server/config/config.js
@@ -2,6 +2,7 @@
  * Módulo en el que realizaremos la configuración global del servicio   *
  ***********************************************************************/
 // Este modulo se ejecutara en el momento en el que lo invoquemos con required desde el servidor principal
+//  Con el podremos configurar las variables para el entorno de desarrollo y el de producción.
 // process es una variable global que esta corriendo todo el tiempo y donde tenemos las variables de entorno
 
 //Configuracion del puerto
